5fc0feff8d86973d6b0f59e3936fe31c3be253fd
prohith8074 <94978427+prohith8074@users.noreply.github.com>
2025-10-16 21:04:43 +0530

    Delete plugins directory

--- plugins/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Plugin system for dynamic component discovery and loading.
-"""
-
-from .registry import PluginRegistry
-
-__all__ = ['PluginRegistry']
--- plugins/query/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Query handler plugins.
-"""
-
-from .vector_plugin import VectorPlugin
-from .graph_plugin import GraphPlugin
-from .filter_plugin import FilterPlugin
-
-__all__ = ['VectorPlugin', 'GraphPlugin', 'FilterPlugin']
